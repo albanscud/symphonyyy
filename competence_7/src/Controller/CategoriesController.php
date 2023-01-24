f73f6f0c02058d9fc8b68515fe11d2b5a65e74ea
scud <alban.escudier@hotmail.fr>
2023-01-24 15:09:30 +0100

    nouveau controlleur categories

---
+++ competence_7/src/Controller/CategoriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/categories', name: 'categories_')]
+class CategoriesController extends AbstractController
+{
+    #[Route('/{slug}', name: 'list')]
+    public function index(Categorie $category): Response
+    {
+        return $this->render('categorie/list.html.twig', compact ('gategory'));
+    }
+     
+}
