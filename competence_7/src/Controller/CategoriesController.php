e191971f1052420d73e0a12ea8a34ff970557543
scud <alban.escudier@hotmail.fr>
2023-01-24 16:36:54 +0100

    ok

--- competence_7/src/Controller/CategoriesController.php
+++ competence_7/src/Controller/CategoriesController.php
@@ -8,13 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 #[Route('/categories', name: 'categories_')]
 class CategoriesController extends AbstractController
 {
+    
     #[Route('/{slug}', name: 'list')]
     public function index(Categorie $category): Response
     {
-        return $this->render('categorie/list.html.twig', compact ('gategory'));
+        return $this->render('categorie/list.html.twig', compact ('category'));
     }
      
 }
