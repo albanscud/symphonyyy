5a1cd4c38a0e05cb6ad8e94763b4ec30f9843e0e
albanscud <113613752+albanscud@users.noreply.github.com>
2023-03-04 14:41:37 +0100

    ajout api coinlayer

--- competence_7/src/Controller/CategoriesController.php
+++ competence_7/src/Controller/CategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +19,30 @@ class CategoriesController extends AbstractController
     {
         $products = $category->getProducts();
 
-        return $this->render('categorie/list.html.twig', compact ('category', 'products'));
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', 'http://api.coinlayer.com/live', [
+            'query' => [
+                'access_key' => 'd730a7e853b07b963e932d922ca9c017',
+            ],
+        ]);
+
+        $data = json_decode($response->getContent(), true);
+        
+    $btcRate = isset($data['rates']) ? $data['rates']['BTC'] : null;
+    $ethRate = isset($data['rates']) ? $data['rates']['ETH'] : null;
+
+
+
+        return $this->render('categorie/list.html.twig', [
+            'data' => [
+                'btcRate' => $btcRate,
+                'ethRate' => $ethRate,
+
+                
+            ],
+            'category' => $category,
+            'products' => $products,
+        ]);
     }
      
 }
